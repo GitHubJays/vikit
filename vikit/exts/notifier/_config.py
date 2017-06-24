7cb8c2ac8dae147bf79e388e7eefcd2efce0ea0a
v1ll4n <188228725@qq.com>
2017-06-24 23:45:04 +0800

    包装client

--- vikit/exts/notifier/_config.py
+++ vikit/exts/notifier/_config.py
@@ -6,27 +6,5 @@
   Created: 06/24/17
 """
 
-########################################################################
-class NotifyConfig(object):
-    """"""
-
-    #----------------------------------------------------------------------
-    def __init__(self):
-        """Constructor"""
-        
-        self._list_adaptor_and_its_config = []
-        
-    #----------------------------------------------------------------------
-    def add_adaptor(self, adaptor_name, config):
-        """"""
-        self._list_adaptor_and_its_config.append((adaptor_name, config))
-    
-    @property
-    def adaptors(self):
-        """"""
-        return [adaptor(params) for adaptor, params in \
-                self._list_adaptor_and_its_config]
-    
-    
     
     
