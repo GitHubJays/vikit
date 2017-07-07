df4e4e70eb24510a3a1ff1164f19e69ed42b840b
root <root@localhost.localdomain>
2017-07-07 21:06:46 +0800

    add result interface and web_test

--- vikit/web/actions.py
+++ vikit/web/actions.py
@@ -14,6 +14,7 @@ from ..api.trigger import get_client_proxy
 
 from flask import render_template, request, jsonify
 from flask import session, redirect, url_for, make_response
+from flask import g
 
 proxy = None
 
@@ -23,7 +24,8 @@ def start():
     """"""
     global proxy
 
-    proxy = get_client_proxy('127.0.0.1', 7000)
+    proxy = get_client_proxy('39.108.169.134', 7000)
+    proxy.regist_result_callback(on_result_feedback)
     return '<h1>Success</h1>'
 
 
@@ -41,7 +43,8 @@ def get_available_module():
     """"""
     global proxy
     proxy.get_available_modules()
-    return json.dumps(proxy.get_available_modules())
+    # return json.dumps(proxy.get_available_modules())
+    return '<p>' + str(proxy.get_available_modules()) + '</p>'
 
 
 @client_app.route('/help/<module_name>')
@@ -70,30 +73,50 @@ def on_select_module(module_name):
     return render_template('main.html', module_help=module_help, selected_module=module_name)
 
 
-@client_app.route('/execute/<module_name>', methods=['POST'])
+@client_app.route('/execute/<module_name>', methods=['get'])
 def execute(module_name):
     """
     get params from form and translate into json then execute
     """
     # def execute(self, module_name, params, offline=False, task_id=None):
     global proxy
+    '''
     target = request.form['target']
     payload = request.form['payload']
     config = json.loads(request.form['config'])
-    task_id = request.form['task_id']
     offline = request.form['offline']
     params = {
         'target': target,
         'payload': payload,
         'config': config,
     }
-    proxy.execute(module_name, params, offline=offline, task_id=task_id)
-    redirect(url_for('/result/' + task_id))
+    '''
+    # proxy.execute(module_name, params, offline=offline, task_id=None)
+    #temp
+    task_id = proxy.execute('demo', {"target": 'http://tbis.me',
+                                     'payload': 'adfa',
+                                     'config': {'param1': True,
+                                                'param2': 'asdfasd'}},
+                            offline=True)
+
+    return '<p>task_id:' + str(task_id) + '</p>'
 
 
-@client_app.route('/result/<task_id>', methods=['GET'])
-def get_result(task_id):
-    '''return the task result according the task_id'''
-    result = None
-    return render_template('result.html', task_id=task_id, result=result)
+result = None
 
+
+def on_result_feedback(result_dict):
+    task_id = result_dict.get('task_id')
+    task_result = result_dict.get('result')
+    global result
+    result = {'task_id': task_id, 'task_result': task_result}
+    # return json.dumps({'task_id': task_id, 'task_result': task_result})
+
+
+@client_app.route('/result/<task_id>', methods=['get'])
+def show_result(task_id):
+    if result != None and result.get('task_id') == task_id:
+        return '<p>task_id:' + result.get('task_id') + '</p><br>' + '<p>task_result:' + str(
+            result.get('task_result')) + '</p>'
+    else:
+        return "<p>Your current task hasn't finished,wait a few seconds</p>"
---
+++ web_test.py
@@ -0,0 +1,3 @@
+from vikit.web.app import client_app
+
+client_app.run('127.0.0.1', 8080, debug=True)
