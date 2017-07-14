89a14e4f7de8e0f5cce9b8d754cb7425c210786f
Conanjun <1526840124@qq.com>
2017-07-14 19:55:39 +0800

    fix start

--- vikit/web/actions.py
+++ vikit/web/actions.py
@@ -24,8 +24,9 @@ def start():
     """"""
     global proxy
 
-    proxy = get_client_proxy('39.108.169.134', 7000)
-    proxy.regist_result_callback(on_result_feedback)
+    if proxy==None:
+        proxy = get_client_proxy('39.108.169.134', 7000)
+        proxy.regist_result_callback(on_result_feedback)
     return 'success' if proxy != None else 'fail'
 
 
